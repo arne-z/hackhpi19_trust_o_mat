c359ea83f9ab5780a7569126cee6dbb0ffb06bf1
Arne <arne.zerndt@student.hpi.de>
2019-05-18 22:45:23 +0200

    Fix routing.

--- trust_o_mat_frontend/src/App.js
+++ trust_o_mat_frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import Header from './components/Header'
-import LandingPage from './components/LandingPage'
-import { Route } from 'react-router-dom'
+import Header from './components/Header';
+import LandingPage from './components/LandingPage';
+import { Route, Switch, Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import Party from './components/Party';
+import PartyList from './components/PartyList';
+import Profile from './components/Profile';
 import Footer from "./components/Footer";
 import './App.scss';
-
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Route path="/" component={LandingPage} />
-      <Route path="/politican/:id" component={Profile} />
-      <Route path="/parties" component={PartyList} />
-      <Route path="/parties/:id" component={Party} />
-      <Footer />
+      <Router history={createBrowserHistory()}>
+        <Header />
+        <Switch>
+          <Route path="/" component={LandingPage} />
+          <Route path="/profile/:id" component={Profile} />
+          <Route path="/parties" component={PartyList} />
+          <Route path="/parties/:id" component={Party} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
