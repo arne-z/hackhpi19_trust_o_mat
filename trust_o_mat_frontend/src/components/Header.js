9eb1fb0e345314385afc20fe5e7ba98efee2c06e
Arne <arne.zerndt@student.hpi.de>
2019-05-18 17:07:04 +0200

    Add Header

---
+++ trust_o_mat_frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.scss'
+
+function Header() {
+    return (
+        <div className="Header">
+            Trust-O-Mat
+        </div>
+    )
+}
+
+export default Header;
