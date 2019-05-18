946537229a550df7695b85bae984386200a4fb88
leogeier <leonard.geier@hotmail.com>
2019-05-19 00:56:05 +0200

    ' -> "

--- trust_o_mat_backend_api/index.js
+++ trust_o_mat_backend_api/index.js
@@ -1,7 +1,7 @@
 const routes = require("./routes");
-const Mongo = require('./db');
-const express = require('express');
-const morgan = require('morgan');
+const Mongo = require("./db");
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
@@ -11,15 +11,15 @@ const db = new Mongo("mongodb://172.16.49.43:27017");
 db.init().then(() => {
     console.log("Initialized Deutsche Bahn!");
 
-    app.use(morgan('short'));
+    app.use(morgan("short"));
 
     app.get("/issues", routes.issues.listIssues.bind(undefined, db));
-    app.get('/parties', routes.parties.listParties.bind(undefined, db));
-    app.get('/parties/:party/metric/:metric', routes.parties.partyMetric.bind(undefined, db));
-    app.get('/politicians', routes.politicians.listPoliticians.bind(undefined, db));
-    app.get('/politicians/:politician/metric/:metric', routes.politicians.politicianMetric.bind(undefined, db));
-    app.get('/stances', routes.stances.listStances.bind(undefined, db));
-    app.get('/votes', routes.votes.listVotes.bind(undefined, db));
+    app.get("/parties", routes.parties.listParties.bind(undefined, db));
+    app.get("/parties/:party/metric/:metric", routes.parties.partyMetric.bind(undefined, db));
+    app.get("/politicians", routes.politicians.listPoliticians.bind(undefined, db));
+    app.get("/politicians/:politician/metric/:metric", routes.politicians.politicianMetric.bind(undefined, db));
+    app.get("/stances", routes.stances.listStances.bind(undefined, db));
+    app.get("/votes", routes.votes.listVotes.bind(undefined, db));
 
     app.use((req, res) => res.status(404).json({message: "404: you done fucked up"}));
 
