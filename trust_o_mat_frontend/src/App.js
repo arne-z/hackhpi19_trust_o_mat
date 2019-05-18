c32d2c0f2f316d59e776fb34a364001a6d74b09e
Arne <arne.zerndt@student.hpi.de>
2019-05-18 22:21:59 +0200

    Add routing.

--- trust_o_mat_frontend/src/App.js
+++ trust_o_mat_frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header'
 import LandingPage from './components/LandingPage'
+import { Route } from 'react-router-dom'
+import Footer from "./components/Footer";
 import './App.scss';
 
 
@@ -8,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <LandingPage />
+      <Route path="/" component={LandingPage} />
+      <Route path="/politican/:id" component={Profile} />
+      <Route path="/parties" component={PartyList} />
+      <Route path="/parties/:id" component={Party} />
+      <Footer />
     </div>
   );
 }
---
+++ trust_o_mat_frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.scss'
+
+function Footer() {
+    return (
+        <div>
+            <div className="Phantom"></div>
+            <div className="Footer">
+                Proof of concept for HackHpi2019 - SAP Challenge 2
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
