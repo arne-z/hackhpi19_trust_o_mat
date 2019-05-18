020836c62b69c44e38ff8c22d67fd832589d75d6
leogeier <leonard.geier@hotmail.com>
2019-05-18 17:36:45 +0200

    Add 404 message

--- trust_o_mat_backend_api/index.js
+++ trust_o_mat_backend_api/index.js
@@ -4,4 +4,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use((req, res) => res.status(404).json({message: "404: you done fucked up"}));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
