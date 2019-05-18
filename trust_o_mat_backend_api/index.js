853bab88706dd234bef1e19bd9fdf8cae90d1174
leogeier <leonard.geier@hotmail.com>
2019-05-19 01:20:08 +0200

    rename

--- trust_o_mat_backend_api/index.js
+++ trust_o_mat_backend_api/index.js
@@ -7,11 +7,11 @@ const argv = require("yargs").argv;
 
 const app = express();
 const port = argv.port;
-const dbIp = argv.dbip;
+const dbAdress = argv.dbAdress;
 
 console.log("Starting Trust-O-Mat backend...");
 
-const db = new Mongo(`mongodb://${dbIp}:27017`);
+const db = new Mongo(`mongodb://${dbAdress}:27017`);
 db.init().then(() => {
     console.log("Initialized Deutsche Bahn!");
 
